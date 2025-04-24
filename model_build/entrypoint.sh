29a7d67ba96d1a3e15e9032cffdf2abfaacbd7d6
gyuhyeong <kgh0730@gmail.com>
2025-04-24 16:38:34 +0900

    WIP: package manager changing

--- model_build/entrypoint.sh
+++ model_build/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+source /app/.venv/bin/activate
+
 API_KEY="$api_key"
 
-exec vllm serve "./google/gemma-3-1b-it" --host "0.0.0.0" --port "8000" --api-key "$API_KEY" --dtype "half" --max-num-seqs "1" "$@"
+exec vllm serve "./google/gemma-3-1b-it" \
+    --host "0.0.0.0" \
+    --port "8000" \
+    --api-key "$VLLM_API_KEY" \
+    --max-num-seqs "1" \
+    "$@"
